1b60886d4fe66a7fb8c65363db68f4ca5ff0c287
osm3030 <115438285+osm3030@users.noreply.github.com>
2025-05-12 02:50:32 +0000

    Crear Setup

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 
 setup(
     name="edu_bidata",
-    author="Andres Callejas",
-    author_email="andres.callejas@iudigital.edu.co",
+    author="Oscar Mantilla",
+    author_email="oscar.mantilla@est.iudigital.edu.co",
     description="",
     install_requires=[
         "pandas",
