a4c2336d0e7a3d47ba76c9378d26db7d2470e590
osm3030 <115438285+osm3030@users.noreply.github.com>
2025-05-12 02:46:24 +0000

    setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name="edu_bidata",
+    author="Andres Callejas",
+    author_email="andres.callejas@iudigital.edu.co",
+    description="",
+    install_requires=[
+        "pandas",
+        "openpyxl",
+        "requests",
+        "BeautifulSoup4"
+    ] 
+)
